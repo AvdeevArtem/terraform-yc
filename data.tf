f769aa3683b4a017f497f8cfc50f7f89385eaf3c
AvdeevArtem <avdeev@mindbox.ru>
2024-01-07 12:52:32 +0800

    fix

--- data.tf
+++ data.tf
@@ -1,9 +1,9 @@
-# data "yandex_vpc_subnet" "default" {
-#     name = "infrastructure-test-ru-central1-a"
-#     depends_on = [
-#       yandex_vpc_subnet.default
-#     ]
-# }
+data "yandex_vpc_subnet" "default" {
+    name = "infrastructure-test-ru-central1-a"
+    depends_on = [
+      yandex_vpc_subnet.default
+    ]
+}
 
 data "terraform_remote_state" "secrets" {
   backend = "s3"
