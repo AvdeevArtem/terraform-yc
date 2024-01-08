7dd2e02f792079acd1f8a5b66f571505c1e7ab1d
AvdeevArtem <avdeev@mindbox.ru>
2024-01-08 12:31:08 +0800

    remove configuration

--- s3.tf
+++ s3.tf
@@ -19,12 +19,12 @@ resource "yandex_iam_service_account_static_access_key" "s3-static-key" {
   description        = "static access key for object storage"
 }
 
-resource "yandex_kms_symmetric_key" "key-s3-bucket" {
-  name              = "s3-bucket-symetric-key"
-  description       = "s3 bucket key"
-  default_algorithm = "AES_128"
-  rotation_period   = "8760h" // equal to 1 year
-}
+# resource "yandex_kms_symmetric_key" "key-s3-bucket" {
+#   name              = "s3-bucket-symetric-key"
+#   description       = "s3 bucket key"
+#   default_algorithm = "AES_128"
+#   rotation_period   = "8760h" // equal to 1 year
+# }
 
 
 resource "yandex_storage_bucket" "s3-bucket" {
@@ -35,12 +35,12 @@ resource "yandex_storage_bucket" "s3-bucket" {
   versioning {
     enabled = true
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = yandex_kms_symmetric_key.key-s3-bucket.id
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       kms_master_key_id = yandex_kms_symmetric_key.key-s3-bucket.id
+  #       sse_algorithm     = "aws:kms"
+  #     }
+  #   }
+  # }
 }
--- vpc.tf
+++ vpc.tf
@@ -39,52 +39,52 @@ locals {
 }
 
 
-resource "yandex_vpc_network" "infrastructure" {
-    description = "Default VPC network"
-    name = "Infrastructure"
-    folder_id = "b1g0e14had2hltv69li5"
-}
+# resource "yandex_vpc_network" "infrastructure" {
+#     description = "Default VPC network"
+#     name = "Infrastructure"
+#     folder_id = "b1g0e14had2hltv69li5"
+# }
 
-resource "yandex_vpc_subnet" "default" {
-  for_each = local.all_subnets
+# resource "yandex_vpc_subnet" "default" {
+#   for_each = local.all_subnets
 
-  name = each.key
-  zone       = each.value.definition.zone
-  network_id = yandex_vpc_network.infrastructure.id
-  v4_cidr_blocks = [each.value.definition.cidr]
-  description    = each.value.definition.purpose
-}
+#   name = each.key
+#   zone       = each.value.definition.zone
+#   network_id = yandex_vpc_network.infrastructure.id
+#   v4_cidr_blocks = [each.value.definition.cidr]
+#   description    = each.value.definition.purpose
+# }
 
-resource "yandex_vpc_security_group" "group1" {
-  name        = "My security group"
-  description = "description for my security group"
-  network_id  = yandex_vpc_network.infrastructure.id
+# resource "yandex_vpc_security_group" "group1" {
+#   name        = "My security group"
+#   description = "description for my security group"
+#   network_id  = yandex_vpc_network.infrastructure.id
 
 
-  labels = {
-    my-label = "my-label-value"
-  }
+#   labels = {
+#     my-label = "my-label-value"
+#   }
 
-  ingress {
-    protocol       = "TCP"
-    description    = "rule1 description"
-    v4_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
-    port           = 8080
-  }
+#   ingress {
+#     protocol       = "TCP"
+#     description    = "rule1 description"
+#     v4_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
+#     port           = 8080
+#   }
 
-  egress {
-    protocol       = "ANY"
-    description    = "rule2 description"
-    v4_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
-    from_port      = 8090
-    to_port        = 8099
-  }
+#   egress {
+#     protocol       = "ANY"
+#     description    = "rule2 description"
+#     v4_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
+#     from_port      = 8090
+#     to_port        = 8099
+#   }
 
-  egress {
-    protocol       = "UDP"
-    description    = "rule3 description"
-    v4_cidr_blocks = ["10.0.1.0/24"]
-    from_port      = 8090
-    to_port        = 8099
-  }
-}
+#   egress {
+#     protocol       = "UDP"
+#     description    = "rule3 description"
+#     v4_cidr_blocks = ["10.0.1.0/24"]
+#     from_port      = 8090
+#     to_port        = 8099
+#   }
+# }
