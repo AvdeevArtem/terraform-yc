c04dceb779d9f96bce7e862483179ffa9a184228
Avdeev Artem <45825379+AvdeevArtem@users.noreply.github.com>
2024-01-08 07:47:15 +0300

    Avdeev/add output ip address (#20)
    * Add output instance ip address
    
    * uncommit vpc and instance
    
    * enable data
    
    ---------
    
    Co-authored-by: AvdeevArtem <avdeev@mindbox.ru>

--- compute.tf
+++ compute.tf
@@ -6,36 +6,35 @@ locals {
   ]
 }
 
-# resource "yandex_compute_instance" "web" {
-#   name = "web"
-#   platform_id = "standard-v3"
-#   zone = var.yandex_zone[0]
+resource "yandex_compute_instance" "web" {
+  name = "web"
+  platform_id = "standard-v3"
+  zone = var.yandex_zone[0]
 
-#   boot_disk {
-#     initialize_params {
-#       image_id = "fd81hgrcv6lsnkremf32"
-#     }
-#   }
+  boot_disk {
+    initialize_params {
+      image_id = "fd81hgrcv6lsnkremf32"
+    }
+  }
 
-#   network_interface {
-#     subnet_id          = data.yandex_vpc_subnet.default.subnet_id
-#     security_group_ids = [yandex_vpc_security_group.group1.id]
-#   }
+  network_interface {
+    subnet_id          = data.yandex_vpc_subnet.default.subnet_id
+  }
 
-#   resources {
-#     cores = 2
-#     memory = 1
-#     core_fraction = 20
-#   }
-#   scheduling_policy {
-#     preemptible = true
-#   }
-#   metadata = {
-#     serial-port-enable = 0
-#   }
+  resources {
+    cores = 2
+    memory = 1
+    core_fraction = 20
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  metadata = {
+    serial-port-enable = 0
+  }
 
-#   allow_stopping_for_update = true
-#   depends_on = [
-#     yandex_vpc_subnet.default
-#   ]
-# }
+  allow_stopping_for_update = true
+  depends_on = [
+    yandex_vpc_subnet.default
+  ]
+}
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "instance_ip_addr" {
+  value =  yandex_compute_instance.web.network_interface.0.ip_address
+}
